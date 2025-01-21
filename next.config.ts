fe47bb54cacb39c7ce2fecde564e2ee0cad81d7c
Thiago Scherer <tharaujo.james@gmail.com>
2025-01-21 12:05:30 -0300

    feat: update next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  basePath: "/huntington",
-  output: "export",
-  reactStrictMode: true,
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
